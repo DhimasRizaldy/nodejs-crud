cd29590b7fe0ea7690a47d25d3c721e3f0196eed
aldii <200401141@student.umri.ac.id>
2023-09-27 21:06:00 +0700

    feat: index

--- 2_node_postgres/helpers/crud.js
+++ 2_node_postgres/helpers/crud.js
@@ -6,8 +6,6 @@ const pool = require('../database/postgres');
 function create(title, body) {
     return new Promise(async (resolve, reject) => {
         try {
-            // "SELECT * FROM posts where id = $1", [1]
-            // "UPDATE posts SET title = $1, body = $2", ["title", "body"]
             let result = await pool.query("INSERT INTO posts (title, body) values ($1, $2) RETURNING *;", [title, body]);
             resolve(result.rows[0]);
         } catch (err) {
@@ -17,7 +15,14 @@ function create(title, body) {
 }
 
 function index() {
-    return posts.data;
+    return new Promise(async (resolve, reject) => {
+        try {
+            let result = await pool.query("SELECT * FROM posts;");
+            resolve(result.rows);
+        } catch (err) {
+            return reject(err);
+        }
+    });
 }
 
 function show(id) {
