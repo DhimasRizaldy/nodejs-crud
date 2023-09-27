3b8d48fe8069ba276394bac65bea95200ead4f02
DhimasRizaldy <dtekchannel02@gmail.com>
2023-09-27 20:28:16 +0700

    dimas:project postgresql

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const { create, index, show, update, destroy } = require('./helpers/crud');
 
 // test create post
-// create('Buku Pemrograman 2', 'Best Practice Pemrograman javascript 2');
+create('Buku Pemrograman 2', 'Best Practice Pemrograman javascript 2');
 
 // Get Data BY ID
 // show(2);
 
 // test show all post
 // test show detail post by id
+// let post = show(1);
+// console.log(post);
 // test update post by id
-update({ id: 1, title: 'Post Instagram Pemandangan', content: 'Konten telah diperbarui.' });
+// update({ id: 1, title: 'Post Instagram Pemandangan', content: 'Konten telah diperbarui.' });
 // test delete post by id
 // destroy(1);
